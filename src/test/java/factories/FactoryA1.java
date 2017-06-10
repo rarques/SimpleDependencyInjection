829662624f1f3d7144912559a296d47d9d91d641
rarques <rarquesvall@gmail.com>
2017-06-10 17:42:01 +0200

    Factories

---
+++ src/test/java/factories/FactoryA1.java
@@ -0,0 +1,7 @@
+package factories;
+
+/**
+ * Created by rarques on 6/10/2017.
+ */
+public class FactoryA1 {
+}
---
+++ src/test/java/factories/FactoryC1.java
@@ -0,0 +1,24 @@
+package factories;
+
+import common.DependencyException;
+import implementations.ImplementationC1;
+import simple.Factory;
+
+/**
+ * Created by rarques on 6/10/2017.
+ */
+public class FactoryC1 implements Factory{
+
+    @Override
+    public ImplementationC1 create(Object... parameters)
+            throws DependencyException {
+        String s;
+        try {
+            s = (String) parameters[0];
+        } catch (ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
+        }
+        return new ImplementationC1(s);
+    }
+
+}
