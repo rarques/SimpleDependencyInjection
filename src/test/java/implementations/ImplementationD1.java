f0b5e52de419458875d681942faf98e7fde3dc17
rarques <rarquesvall@gmail.com>
2017-06-10 14:04:18 +0200

    classes and interfaces for testing

---
+++ src/test/java/implementations/ImplementationD1.java
@@ -0,0 +1,16 @@
+package implementations;
+
+import interfaces.InterfaceD;
+
+public class ImplementationD1 implements InterfaceD {
+
+    private int i;
+
+    public ImplementationD1(int i) {
+        this.i = i;
+    }
+
+    public int getI() {
+        return i;
+    }
+}
