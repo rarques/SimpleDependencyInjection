ae1bbb2d9ab0cb4d2dca72f31372a08b637141e4
rarques <rarquesvall@gmail.com>
2017-06-15 18:59:35 +0200

    Factory implementationC1

---
+++ src/test/java/factories/complex/FactoryC1.java
@@ -0,0 +1,25 @@
+package factories.complex;
+
+import common.DependencyException;
+import complex.Factory;
+import implementations.ImplementationC1;
+import implementations.ImplementationD1;
+
+/**
+ * Created by rarques on 6/15/2017.
+ */
+public class FactoryC1 implements Factory<ImplementationC1> {
+
+    @Override
+    public ImplementationC1 create(Object... parameters)
+            throws DependencyException {
+        String s;
+        try {
+            s = (String) parameters[0];
+        } catch (ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
+        }
+        return new ImplementationC1(s);
+    }
+
+}
